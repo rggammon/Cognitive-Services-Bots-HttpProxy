e5b81391cdaafb94796df5b9b8d4eae895f56ef3
Ryan Gammon <rgammon@microsoft.com>
2020-02-04 10:45:01 -0800

    Asp.net Core Web Api template

---
+++ StreamingBotHttpProxy/Controllers/ProxyController.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace StreamingBotHttpProxy.Controllers
+{
+    [ApiController]
+    public class ProxyController : ControllerBase
+    {
+        [HttpGet]
+        [Route("api/messages")]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
